3a29f29d39d3ee33eab02d81c1ab2633c7d7e3fe
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-01-24 00:09:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  musicals = [hansen, hamilton, phantom, cats]
+  musicals = ["hansen", "hamilton", "phantom", "cats"]
 end
 
 def add_element_to_end_of_array(array, element)
